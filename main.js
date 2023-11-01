6cdee98f10048a117ad9ad2fa3720ceb09e54c2a
Felipe Bassi <fkb5105@psu.edu>
2023-11-01 16:31:58 -0400

    Updates

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
 
-
+/* AAAAAAA */ 
 const scene = new THREE.Scene();
         
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
